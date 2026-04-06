0169bcd502b6661dcd29cfab80a38b004a224edf
Dhalintin <uchexdhalitin@gmail.com>
2026-04-06 12:06:57 +0100

    Chore: modified the styling to the cards on smaller screens

--- src/utils/gridStyle.ts
+++ src/utils/gridStyle.ts
@@ -3,38 +3,38 @@ export const gameGrid = [
     outter:
       "grid w-full mx-auto h-full max-w-xl grid-cols-3 gap-2 max-w-screen",
     inner:
-      "relative w-[45%] h-[60%] md:w-full md:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
+      "relative w-[45%] h-[60%] lg:w-full lg:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
   },
   {
     outter: "grid w-full mx-auto h-full max-w-xl grid-cols-4 gap-3",
     inner:
-      "relative w-[35%] h-[55%] md:w-full md:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
+      "relative w-[35%] h-[55%] lg:w-full lg:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
   },
   {
     outter: "grid w-full mx-auto h-full max-w-xl grid-cols-4 gap-1",
     inner:
-      "relative w-[50%] h-[60%] md:w-full md:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
+      "relative w-[50%] h-[60%] lg:w-full lg:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
   },
   {
     outter: "grid w-full mx-auto h-full max-w-md grid-cols-4 gap-1",
     inner:
-      "relative w-[70%] h-[90%] md:w-full md:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
+      "relative w-[70%] h-[90%] lg:w-full lg:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
   },
   {
     outter: "grid w-full mx-auto h-full max-w-lg grid-cols-5 gap-1",
     inner:
-      "relative w-[55%] h-[70%] md:w-full md:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
+      "relative w-[55%] h-[70%] lg:w-full lg:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
   },
   {
     outter: "grid w-full mx-auto h-full max-w-xl grid-cols-6 gap-1",
     inner:
-      "relative w-[45%] h-[60%] md:w-full md:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
+      "relative w-[45%] h-[60%] lg:w-full lg:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
   },
   {
     outter:
       "grid w-full mx-auto h-full max-w-3xl md:grid-cols-8 grid-cols-7 gap-1",
     inner:
-      "relative w-[50%] h-[60%] md:w-full md:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
+      "relative w-[50%] h-[60%] lg:w-full lg:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
   },
   {
     outter:
@@ -46,18 +46,18 @@ export const gameGrid = [
     outter:
       "grid w-full mx-auto h-full max-w-5xl md:grid-cols-10 grid-cols-8 gap-1",
     inner:
-      "relative w-[43%] h-[65%] md:w-full md:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
+      "relative w-[43%] h-[65%] lg:w-full lg:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
   },
   {
     outter:
       "grid w-full mx-auto h-full max-w-6xl md:grid-cols-12 grid-cols-8 gap-1",
     inner:
-      "relative w-[50%] h-[75%] md:w-full md:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
+      "relative w-[50%] h-[75%] lg:w-full lg:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
   },
   {
     outter:
       "grid w-full mx-auto h-full max-w-7xl md:grid-cols-13 grid-cols-8 gap-2",
     inner:
-      "relative w-[65%] h-[80%] md:w-full md:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
+      "relative w-[65%] h-[80%] lg:w-full lg:h-full rounded-md transition-transform duration-700 transform-style-preserve-3d gap-2",
   },
 ];
